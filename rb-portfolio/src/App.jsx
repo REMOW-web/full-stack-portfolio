6bd0a934ecae123fe3841291e1f274d0f42897b0
Remoneilwe Rammekwa <rrammekwa@jhb.dvt.co.za>
2025-06-24 20:59:10 +0200

    Add project files directly instead of as submodule

---
+++ rb-portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import NavbarMain from "./components/navbar/NavbarMain"
+
+function App() {
+  return (
+    <main className = "font-body">
+      <NavbarMain />
+    </main>
+  );
+}
+
+export default App;
